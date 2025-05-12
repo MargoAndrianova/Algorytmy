2c9fe6985440880ed0e53e77444a2d58149fcd5e
Margo <rita.anova@gmail.com>
2025-05-12 11:25:07 +0300

    Hahaton

---
+++ HW_24/HW_24_19_213.py
@@ -0,0 +1,74 @@
+from collections import deque
+
+def read_input():
+    H, W = map(int, input().split())
+    grid = [list(input().rstrip()) for _ in range(H)]
+    start1 = start2 = exit_pos = None
+    for i in range(H):
+        for j in range(W):
+            c = grid[i][j]
+            if c == '1':
+                start1 = (i, j); grid[i][j] = '.'
+            elif c == '2':
+                start2 = (i, j); grid[i][j] = '.'
+            elif c == '*':
+                exit_pos = (i, j); grid[i][j] = '.'
+    return H, W, grid, start1, start2, exit_pos
+
+def bfs_escape(H, W, grid, s1, s2, exit_pos):
+    # Тепер dirs у впорядкованому (лексикографічному) порядку
+    dirs = [
+        ('D',  1, 0),
+        ('L',  0,-1),
+        ('R',  0, 1),
+        ('U', -1, 0),
+    ]
+    start = (s1[0], s1[1], s2[0], s2[1])
+    goal  = (exit_pos[0], exit_pos[1], exit_pos[0], exit_pos[1])
+
+    dq = deque([start])
+    parent = {start: None}
+
+    while dq:
+        state = dq.popleft()
+        if state == goal:
+            return parent, True
+        y1, x1, y2, x2 = state
+        for cmd, dy, dx in dirs:
+            ny1, nx1 = move_one(y1, x1, dy, dx, H, W, grid)
+            ny2, nx2 = move_one(y2, x2, dy, dx, H, W, grid)
+            new_state = (ny1, nx1, ny2, nx2)
+            if new_state not in parent:
+                parent[new_state] = (state, cmd)
+                dq.append(new_state)
+
+    return parent, False
+
+def move_one(y, x, dy, dx, H, W, grid):
+    ny, nx = y+dy, x+dx
+    if 0 <= ny < H and 0 <= nx < W and grid[ny][nx] != '#':
+        return ny, nx
+    else:
+        return y, x
+
+def extract_path(parent, exit_pos):
+    goal = (exit_pos[0], exit_pos[1], exit_pos[0], exit_pos[1])
+    if goal not in parent:
+        return None
+    path = []
+    cur = goal
+    while parent[cur] is not None:
+        prev_state, cmd = parent[cur]
+        path.append(cmd)
+        cur = prev_state
+    path.reverse()
+    return path
+
+H, W, grid, s1, s2, exit_pos = read_input()
+parent, ok = bfs_escape(H, W, grid, s1, s2, exit_pos)
+if not ok:
+    print(-1)
+else:
+    path = extract_path(parent, exit_pos)
+    print(len(path))
+    print(''.join(path))
---
+++ HW_24/HW_24_2223.py
@@ -0,0 +1,55 @@
+from collections import deque
+
+def read():
+    h, m, n = map(int, input().split())
+    levels = []
+    for lvl in range(h):
+        level = []
+        for _ in range(m):
+            level.append(list(input().rstrip()))
+        levels.append(level)
+        if lvl != h - 1:
+            input()
+    return h, m, n, levels
+
+h, m, n, lab = read()
+
+start = None
+end = None
+for z in range(h):
+    for x in range(m):
+        for y in range(n):
+            if lab[z][x][y] == '1':
+                start = (z, x, y)
+                lab[z][x][y] = '.'
+            elif lab[z][x][y] == '2':
+                end = (z, x, y)
+                lab[z][x][y] = '.'
+
+dirs = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+visited = [[[False for _ in range(n)] for _ in range(m)] for _ in range(h)]
+q = deque()
+q.append((*start, 0))  # (z, x, y, time)
+visited[start[0]][start[1]][start[2]] = True
+
+while q:
+    z, x, y, time = q.popleft()
+
+    if (z, x, y) == end:
+        print(time)
+        break
+
+    # Горизонтальні переміщення
+    for dx, dy in dirs:
+        nx, ny = x + dx, y + dy
+        if 0 <= nx < m and 0 <= ny < n:
+            if lab[z][nx][ny] == '.' and not visited[z][nx][ny]:
+                visited[z][nx][ny] = True
+                q.append((z, nx, ny, time + 5))
+
+    # Переміщення вниз
+    if z + 1 < h:
+        if lab[z + 1][x][y] == '.' and not visited[z + 1][x][y]:
+            visited[z + 1][x][y] = True
+            q.append((z + 1, x, y, time + 5))
